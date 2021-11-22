b6511359ed505c33e20e742d1d50dee9a7319ed5
WuChenDi <996194720@qq.com>
2021-11-22 15:36:54 +0800

    docs: ele 源码架构(git- | release)，自动化脚本，分支diff与commit等操作

--- build/git-release.sh
+++ build/git-release.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 git checkout dev
 
+# 这里主要是和远程的 dev 分支做 diff 然后合并
+
 if test -n "$(git status --porcelain)"; then
   echo 'Unclean working tree. Commit or stash changes first.' >&2;
   exit 128;
